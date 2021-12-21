d6497de2a45760d51afb816297dc56c44e907b46
Jose Denes <josedenes17@gmail.com>
2021-12-21 15:15:00 -0300

    Backend criado

---
+++ backend/src/main/java/br/com/zup/desafio/marvel/MarvelApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafio.marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApplication.class, args);
+	}
+
+}
