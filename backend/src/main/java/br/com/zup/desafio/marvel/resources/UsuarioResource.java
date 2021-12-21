5c74678faf7100bf3123bb6c907dc9009cf2228c
Jose Denes <josedenes17@gmail.com>
2021-12-21 18:52:32 -0300

    Inserindo usuario

--- backend/src/main/java/br/com/zup/desafio/marvel/resources/UsuarioResource.java
+++ backend/src/main/java/br/com/zup/desafio/marvel/resources/UsuarioResource.java
@@ -1,13 +1,17 @@
 package br.com.zup.desafio.marvel.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.zup.desafio.marvel.dto.UsuarioDTO;
 import br.com.zup.desafio.marvel.entities.Usuario;
@@ -33,5 +37,13 @@ public class UsuarioResource {
 		UsuarioDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<UsuarioDTO> insert(@RequestBody UsuarioDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO tb_usuario (nome, email, cpf, data_nascimento ) VALUES ('jose', 'jose@gmail.com', '000.000.000-00', NOW());
+INSERT INTO tb_usuario (nome, email, cpf, data_nascimento ) VALUES ('joao', 'joao@gmail.com', '000.000.000-00', NOW());
+INSERT INTO tb_usuario (nome, email, cpf, data_nascimento ) VALUES ('pedro', 'pedro@gmail.com', '000.000.000-00', NOW());
